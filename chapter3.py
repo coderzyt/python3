1723fbcf3d242288706ea13135f3a4b1c2522f15
yuntzhao <yuntzhao@ebay.com>
2019-11-29 09:57:34 +0800

    add files

--- chapter3.py
+++ chapter3.py
@@ -1,44 +1,62 @@
-def read_txt_from_file(file):
-    return file
+import sys
+import optparse
 
-
-def read_txt(text):
-    read_list = text.split()
-    dic = {}
-    for every_word in read_list:
-        if dic.__contains__(every_word):
-            dic[every_word] += 1
+def wordcount(readtxt):
+    dict = {}
+    readlist = readtxt.split()
+    for everyword in readlist:
+        if everyword in dict:
+            dict[everyword]+=1
         else:
-            dic.setdefault(every_word, 1)
-    print(dic)
+            dict[everyword] = 1
+    return dict
 
-def test_cmp(stext, dtext):
+def testcmp(stext, dtext):
     cmplist = []
-    slineslist = []
-
-
-def getAll(*args):
-    print(args)
-
-def getAlls(**args):
-    print(args)
-
-def funexe(keyparam, choice=1, *args, **keywords):
-    print(keyparam, choice, args, keywords)
-
-f = lambda a: (a and 1 or 2)
-
+    sLinesList = []
+    for line in stext.splitlines():
+        if not line.isspace() and line != "":
+            sLinesList.append(line)
+    dLinesList = []
+    for line in dtext.splitlines():
+        if not line.isspace() and line != "":
+            dLinesList.append(line)
+    sLen = len(sLinesList)
+    dLen = len(dLinesList)
+    for step in range(max(sLen, dLen)):
+        try:
+            sWordList = sLinesList[step].split()
+        except IndexError as e:
+            print("dFile is end")
+            sLinesList.append("")
+        try:
+            dWordList = dLinesList[step].split()
+        except IndexError as e:
+            print("dFile is end")
+            dLinesList.append("")
+        if sWordList != dWordList:
+            cmplist.append((step, sLinesList[step], dLinesList[step]))
+    return cmplist
 
 if __name__ == '__main__':
-    # text = 'test test deploy compile return of the king king king'
-    # read_txt(text)
-    # getAll(1)
-    # getAll(1, 2)
-    #
-    # getAlls(one=1)
-    # getAlls(one=1, two=2)
-    # funexe('a', 'b', 'c', 'd')
-    # funexe('a', 1, 'c', 'd')
-    # funexe('a', 1, 'c', 'd', three=3)
+    parser = optparse.OptionParser()
+    parser.add_option("-s", "--sFile", action="store", type="string", dest="sFileName")
+    parser.add_option("-d", "--dFile", action="store", type="string", dest="dFileName")
+    (options, args) = parser.parse_args()
+    with open(options.sFileName, 'r') as sFile, open(options.dFileName, 'r') as dFile:
+        sText = sFile.read()
+        dText = dFile.read()
 
-    print(f(False))
+        print("文件: %s" %options.sFileName)
+        print("词汇总数: %d" %len(wordcount(sText)))
+        print("各词汇统计: %s" %wordcount(sText))
+
+        print("文件: %s" % options.dFileName)
+        print("词汇总数: %d" % len(wordcount(dText)))
+        print("各词汇统计: %s" % wordcount(dText))
+
+        cmpList = testcmp(sText, dText)
+        for diff in cmpList:
+            print("%s %s: %s" %(options.sFileName, diff[0], diff[1]))
+            print("%s %s: %s" %(options.dFileName, diff[0], diff[2]))
+
--- chapter5.py
+++ chapter5.py
@@ -1,3 +1,5 @@
+import optparse
+
 class customer(object):
     buy_dict = {}
 
@@ -6,18 +8,16 @@ class customer(object):
 
 
 class testA(object):
-    def __init__(self):
-        print("created")
-
-    def __del__(self):
-        print("deleted")
-
+    # def __init__(self):
+    #     print("created")
+    #
+    # def __del__(self):
+    #     print("deleted")
     value = 0
 
     def printf(self):
         print(self)
         print(self.value)
-
 
 class testB(object):
     def printf(self):
@@ -38,9 +38,174 @@ class vipcust(object):
     def buy_some(self, prod_name, price):
         disct_price = self._disct(prod_name, price)
         self.prod_dict[prod_name]=disct_price
-    
 
+class A(object):
+    def printf(self):
+        print("A")
+
+class B(object):
+    def printf(self):
+        print("B")
+
+class C(A, B):
+    def printf(self):
+        print("C")
+        A.printf(self)
+        B.printf(self)
+
+class picprint(object):
+    def __init__(self):
+        self.list_total = [[] for x in range(9)]
+
+    def getprintstr(self, string):
+        self.num_str = string
+
+    def __unionpic(self, prc_list):
+        for step in range(9):
+            self.list_total[step] += [","] + prc_list[step]
+
+    def printstr(self):
+        num_fact = numfact()
+        for eve_char in self.num_str:
+            num_obj =num_fact.factory(eve_char)
+            self.__unionpic(num_obj.draw())
+            print_str = ''
+            for sub_list in self.list_total:
+                for every_char in sub_list:
+                    print_str += every_char
+                print_str = '\n'
+        print(print_str)
+
+class numfact(object):
+    def factory(self, which):
+        if int(which) == 0:
+            return zeropic()
+        elif int(which) == 1:
+            return onepic()
+        elif int(which) == 2:
+            return twopic()
+        elif int(which) == 3:
+            return threepic()
+        elif int(which) == 4:
+            return fourpic()
+        elif int(which) == 5:
+            return fivepic()
+        elif int(which) == 6:
+            return sixpic()
+        elif int(which) == 7:
+            return sevenpic()
+        elif int(which) == 8:
+            return eightpic()
+        elif int(which) == 9:
+            return ninepic()
+
+class numpic(object):
+    def __init__(self):
+        self.pic_list = [['' for i in range(9)] for x in range(9)]
+
+    def setpos(self, i, j):
+        self.pic_list[i][j] = '*'
+
+    def draw(self):
+        return self.pic_list
+
+    def drawline(self, line):
+        for step in range(9):
+            self.setpos(line, step)
+
+    def drawrow(self, row, row_type):
+        if row_type == 0:
+            for step in range(9):
+                self.setpos(step, row)
+        elif row_type == 1:
+            for step in range(5):
+                self.setpos(step, row)
+        else:
+            for step in range(4, 9):
+                self.setpos(step, row)
+
+class zeropic(numpic):
+    def draw(self):
+        self.drawline(0)
+        self.drawrow(0, 0)
+        self.drawline(8)
+        self.drawrow(8, 0)
+        return self.pic_list
+
+class onepic(numpic):
+    def draw(self):
+        self.drawrow(8, 0)
+        return self.pic_list
+
+class twopic(numpic):
+    def draw(self):
+        self.drawline(0)
+        self.drawrow(8, 1)
+        self.drawline(4)
+        self.drawrow(0, 2)
+        self.drawline(8)
+        return self.pic_list
+
+class threepic(numpic):
+    def draw(self):
+        self.drawline(0)
+        self.drawrow(8, 0)
+        self.drawline(4)
+        self.drawline(8)
+        return self.pic_list
+
+class fourpic(numpic):
+    def draw(self):
+        self.drawrow(0, 1)
+        self.drawline(4)
+        self.drawrow(8, 0)
+        return self.pic_list
+
+class fivepic(numpic):
+    def draw(self):
+        self.drawline(0)
+        self.drawrow(0, 1)
+        self.drawline(4)
+        self.drawrow(8, 2)
+        self.drawline(8)
+        return self.pic_list
+
+class sixpic(numpic):
+    def draw(self):
+        self.drawline(0)
+        self.drawrow(0, 0)
+        self.drawline(4)
+        self.drawrow(8, 2)
+        self.drawline(8)
+        return self.pic_list
+
+class sevenpic(numpic):
+    def draw(self):
+        self.drawline(0)
+        self.drawrow(8, 0)
+        return self.pic_list
+
+class eightpic(numpic):
+    def draw(self):
+        self.drawline(0)
+        self.drawrow(0, 0)
+        self.drawrow(8, 0)
+        self.drawline(4)
+        self.drawline(8)
+        return self.pic_list
+
+class ninepic(numpic):
+    def draw(self):
+        self.drawline(0)
+        self.drawrow(0, 1)
+        self.drawrow(8, 0)
+        self.drawline(4)
+        return self.pic_list
 
 if __name__ == '__main__':
-    a = testA()
-    a.printf()
+    parser = optparse.OptionParser
+    parser.add_option("-n", "--number", action="store", type="string", dest="num")
+    option, args = parser.parse_args()
+    print_str = picprint
+    print_str.getprintstr(option.num)
+    print_str.printstr()
