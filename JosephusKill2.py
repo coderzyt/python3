9b16c5804610ba1a484718c9f8a3553a896ffc52
clay zhao <clay@MacbookPro.local>
2019-12-03 00:21:45 +0800

    remove JosephusKill2

--- JosephusKill2.py
+++ JosephusKill2.py
@@ -1,20 +1,20 @@
-class JosephusKill2(object):
-
-    def josephus_kill_2(self, head, m):
-        if head is None or head.next is None or m < 1:
-            return head
-        cur = head.next
-        tmp = 1
-        while cur != head:
-            tmp = tmp + 1
-            cur = cur.next
-        tmp = self.get_live(tmp, m)
-        while (--tmp != 0):
-            head = head.next
-        head.next = head
-        return head
-
-    def get_live(self, i, m):
-        if i == 1:
-            return 1;
-        return (self.get_live(i-1, m) + m - 1) % 1 + 1
+# class JosephusKill2(object):
+#
+#     def josephus_kill_2(self, head, m):
+#         if head is None or head.next is None or m < 1:
+#             return head
+#         cur = head.next
+#         tmp = 1
+#         while cur != head:
+#             tmp = tmp + 1
+#             cur = cur.next
+#         tmp = self.get_live(tmp, m)
+#         while (--tmp != 0):
+#             head = head.next
+#         head.next = head
+#         return head
+#
+#     def get_live(self, i, m):
+#         if i == 1:
+#             return 1;
+#         return (self.get_live(i-1, m) + m - 1) % 1 + 1
